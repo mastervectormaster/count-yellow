2c35e5de9706c5eaf7b25b0015df2c34f2a07747
mastervectormaster <vectormaster123456789@gmail.com>
2022-08-02 21:56:24 +0900

    added extract.py

---
+++ extract.py
@@ -0,0 +1,27 @@
+import cv2
+import glob
+import os
+
+lst_videos = glob.glob("auto labels/*.mp4")
+
+i = 0
+save_idx = 0
+for v in lst_videos:
+    vid = cv2.VideoCapture(v)
+    basename = os.path.basename(v)
+    print(basename)
+    basename = basename.replace(".mp4", "")
+    while True:
+        ret, frame = vid.read()
+        if ret:
+            if i == 35:
+                save_path = "label_img/{}_{}.jpg".format(basename, save_idx)
+                print("     Save ", save_path)
+                cv2.imwrite(save_path, frame)
+                save_idx += 1
+                i = 0
+            else:
+                i += 1
+        else:
+            save_idx = 0
+            break
